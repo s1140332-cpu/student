5803cf750bbe9753cefbaaad88223ef6b79b6502
s1140332-cpu <s1140332@dmhs.kh.edu.tw>
2025-10-17 16:22:44 +0800

    hw2

---
+++ hw2.cpp
@@ -0,0 +1,12 @@
+#inclube<iostream>
+#include <string>
+
+int main() {
+    int age=57
+    double height=170.5;
+    std::string name= "Hank"
+    Bool is_pass=ture;
+    std::cout << "趙均翰: "<<name<<"180:"<<height>> std::endl;
+    std::cout << "年齡: "<<age<<"是否及格:" <<is_pass<<std::endl;
+    return 0;
+}
