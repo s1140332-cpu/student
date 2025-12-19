1e597ddd5b20bacb7fe265c90340c1d4a6cd0ea1
s1140332-cpu <s1140332@dmhs.kh.edu.tw>
2025-12-19 16:16:22 +0800

    HW14

--- main.cpp
+++ main.cpp
@@ -1,33 +1,21 @@
 #include <iostream>
-using namespace std;
 
-int main() {
 
-    string name[5] = {"Amy", "Ben", "Cindy", "David", "Eva"};
+int main ()
+{
+    int score[3] = {95,68,42};
+    int temp;
 
-    int score[5][3] = {
-        {80, 90, 85},
-        {70, 75, 78},
-        {88, 92, 95},
-        {60, 65, 70},
-        {90, 93, 89}
-    };
-
-    cout << "姓名\t國文\t英文\t數學\t總分\t平均\n";
-
-    for (int i = 0; i < 5; i++) {
-        int sum = 0;
-
-        cout << name[i] << "\t";
-
-        for (int j = 0; j < 3; j++) {
-            cout << score[i][j] << "\t";
-            sum += score[i][j];
-        }
-
-        double avg = sum / 3.0;
-        cout << sum << "\t" << avg << endl;
+    temp = score[2];
+    score[2] = score[0];
+    score[0] = temp;
+    for (int i=0;i<3;i++) {
+        std::cout << score[i]<< " ";
     }
-
+    
     return 0;
+
 }
+    
+
+
